69f1f0749cff21a76a3c4d63b7cc7770aa46da3c
mugala <mugalalevi21@gmail.com>
2018-09-11 16:09:40 +0300

    update post This commit will check the id of the post and update it to what the user wants

--- microposts/src/app.js
+++ microposts/src/app.js
@@ -28,23 +28,38 @@ function getPosts(){
 function submitPost(){
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
+
+  const data = {
+    title,
+    body
+  } 
 
   if(title === '' || body === ''){
     ui.showAlert('Please fill in all fields', 'alert alert-danger');
   }else{
-    
-    const data = {
-      title,
-      body
-    }  
-    //Create post
+
+    //Check for id
+    if(id === ''){
+      //Create post
     http.post('http://localhost:3000/posts', data)
-     .then(data =>{
-       ui.showAlert('Post has been added','alert alert-success');
-       ui.clearFields(); 
-      getPosts();
-     })
-     .catch(err => console.log(err));
+    .then(data =>{
+      ui.showAlert('Post has been added','alert alert-success');
+      ui.clearFields(); 
+     getPosts();
+    })
+    .catch(err => console.log(err));
+    }else{
+      //update post
+    http.put(`http://localhost:3000/posts/${id}`, data)
+    .then(data =>{
+      ui.showAlert('Post has been updated','alert alert-success');
+      ui.changeFormState('add');
+     getPosts();
+    })
+    .catch(err => console.log(err));
+
+    }
   }
 }
 
